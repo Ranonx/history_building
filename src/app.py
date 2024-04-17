c9fc92b4b7024398952f54020ffe87dac93dc70e
Ranonx <108208227+Ranonx@users.noreply.github.com>
2024-04-17 15:01:34 +0800

    added requirements

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, render_template,json
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
@@ -12,6 +14,10 @@ def main():
         geojson_data = json.load(file)
     # Pass the loaded data to the template
     return render_template('base.html', geojson_data=geojson_data)
+
+def load_initialize_data():
+    db.create_all()  # Ensure all tables are created
+
 
 if __name__ == '__main__':
     app.run(debug=True)
